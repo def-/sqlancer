933b9fa077916fa8f431612d6892f6fa4c711a78
Gabor Gevay <gabor@materialize.com>
2026-05-24 15:30:27 +0200

    Remove more flags that have been or will soon be removed
    See https://github.com/MaterializeInc/database-issues/issues/8780

--- src/sqlancer/materialize/gen/MaterializeSetGenerator.java
+++ src/sqlancer/materialize/gen/MaterializeSetGenerator.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 public class MaterializeSetGenerator {
 
-    private static final String[] OPTIMIZER_FLAGS = { "enable_eager_delta_joins", "enable_reduce_mfp_fusion",
-            "enable_variadic_left_join_lowering", "enable_cast_elimination", "enable_new_outer_join_lowering",
-            "enable_cardinality_estimates", "enable_join_prioritize_arranged",
-            "enable_projection_pushdown_after_relation_cse", "enable_eq_classes_withholding_errors",
-            "enable_consolidate_after_union_negate", "enable_letrec_fixpoint_analysis",
-            "enable_less_reduce_in_eqprop", "enable_dequadratic_eqprop_map" };
+    private static final String[] OPTIMIZER_FLAGS = {
+            "enable_eager_delta_joins",
+            "enable_variadic_left_join_lowering",
+            "enable_cast_elimination",
+            "enable_new_outer_join_lowering",
+            "enable_eq_classes_withholding_errors",
+            "enable_less_reduce_in_eqprop",
+            "enable_dequadratic_eqprop_map"
+    };
 
     private static final String PERSIST_FAST_PATH_LIMIT = "persist_fast_path_limit";
     private static final int[] PERSIST_FAST_PATH_LIMIT_VALUES = { 0, 1, 25, 1000 };
